4956b8f7e8c3b3a44536bef5192b5a708e8b6843
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-05-31 18:46:16 -0600

    Uso de *ptr

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -11,26 +11,29 @@ int main(void){
     y=6.7;
     z='@';
 
-    int* ptrInt;//Variable int
-    float* ptrFloat;//Variable float
-    char* ptrChar;//Variable char
+    int* ptrInt;//Apuntador int
+    float* ptrFloat;//Apuntador float
+    char* ptrChar;//Apuntador char
 
-    ptrInt = &y;//Guardar la direccion de variable y en variable ptrInt
-    ptrFloat = &z;//Guardar la direccion de variable z en variable ptrFloat
-    ptrChar = &x;//Guardar la direccion de variable x en variable ptrChar
+    ptrInt = &x;//Guardar el dato en variable x en Apuntador ptrInt
+    ptrFloat = &y;//Guardar el dato en variable y en Apuntador ptrFloat
+    ptrChar = &z;//Guardar el dato en variable z en Apuntador ptrChar
 
     cout<<"x\t  = "<<x<<endl//Contenido en x
         <<"&x\t  = "<<&x<<endl//Direccion de x
         <<"ptrInt\t  = "<<ptrInt<<endl//Contenido en ptrInt
-        <<"&ptrInt\t  = "<<&ptrInt<<endl<<endl//Direccion de ptrInt
+        <<"&ptrInt\t  = "<<&ptrInt<<endl//Direccion de ptrInt
+        <<"*ptrInt\t  = "<<*ptrInt<<endl<<endl//Dato contenido en direccion contenida en ptrInt
         <<"y\t  = "<<y<<endl//Contenido en y
         <<"&y\t  = "<<&y<<endl//Direccion de y
         <<"ptrFloat  = "<<ptrFloat<<endl//Contenido en ptrFloat
-        <<"&ptrFloat = "<<&ptrFloat<<endl<<endl//Direccion de ptrFloat
+        <<"&ptrFloat = "<<&ptrFloat<<endl//Direccion de ptrFloat
+        <<"*ptrFloat = "<<*ptrFloat<<endl<<endl//Dato contenido en direccion contenida en ptrFloat
         <<"z\t  = "<<z<<endl//Contenido en z
         <<"&z\t  = "<<(void*)&z<<endl//Direccion de z
         <<"ptrChar\t  = "<<(void*)ptrChar<<endl//Contenido en ptrChar
-        <<"&ptrChar  = "<<(void*)&ptrChar<<endl;//Direccion de ptrChar
+        <<"&ptrChar  = "<<(void*)&ptrChar<<endl//Direccion de ptrChar
+        <<"*ptrChar  = "<<*ptrChar<<endl<<endl;//Dato contenido en direccion contenida en ptrChar
 
     return 0;
 }
