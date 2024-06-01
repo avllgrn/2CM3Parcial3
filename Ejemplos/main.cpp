abf5def1bea272e614483630043b1b4fc9fb3c8c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-05-31 18:54:04 -0600

    Apuntadores a objetos

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
 #include <stdio.h>
+#include "Punto2D.h"
+#include "Complejo.h"
+#include "Fraccion.h"
 using namespace std;
 
 int main(void){
-    int x;//Variable int
-    float y;//Variable float
-    char z;//Variable char
+    Punto2D P;//Objeto Punto2D
+    Complejo C;//Objeto Complejo
+    Fraccion F;//Objeto Fraccion
 
-    x=5;
-    y=6.7;
-    z='@';
+    Punto2D* ptrPunto2D;//Apuntador a Punto2D
+    Complejo* ptrComplejo;//Apuntador a Complejo
+    Fraccion* ptrFraccion;//Apuntador a Fraccion
 
-    int* ptrInt;//Apuntador int
-    float* ptrFloat;//Apuntador float
-    char* ptrChar;//Apuntador char
+    ptrPunto2D = &P;//Guardar la direccion de Objeto Punto2D y en Apuntador a ptrPunto2D
+    ptrComplejo = &C;//Guardar la direccion de Objeto Complejo en Apuntador a ptrComplejo
+    ptrFraccion = &F;//Guardar la direccion de Objeto Fraccion en Apuntador a ptrFraccion
 
-    ptrInt = &x;//Guardar el dato en variable x en Apuntador ptrInt
-    ptrFloat = &y;//Guardar el dato en variable y en Apuntador ptrFloat
-    ptrChar = &z;//Guardar el dato en variable z en Apuntador ptrChar
 
-    cout<<"x\t  = "<<x<<endl//Contenido en x
-        <<"&x\t  = "<<&x<<endl//Direccion de x
-        <<"ptrInt\t  = "<<ptrInt<<endl//Contenido en ptrInt
-        <<"&ptrInt\t  = "<<&ptrInt<<endl//Direccion de ptrInt
-        <<"*ptrInt\t  = "<<*ptrInt<<endl<<endl//Dato contenido en direccion contenida en ptrInt
-        <<"y\t  = "<<y<<endl//Contenido en y
-        <<"&y\t  = "<<&y<<endl//Direccion de y
-        <<"ptrFloat  = "<<ptrFloat<<endl//Contenido en ptrFloat
-        <<"&ptrFloat = "<<&ptrFloat<<endl//Direccion de ptrFloat
-        <<"*ptrFloat = "<<*ptrFloat<<endl<<endl//Dato contenido en direccion contenida en ptrFloat
-        <<"z\t  = "<<z<<endl//Contenido en z
-        <<"&z\t  = "<<(void*)&z<<endl//Direccion de z
-        <<"ptrChar\t  = "<<(void*)ptrChar<<endl//Contenido en ptrChar
-        <<"&ptrChar  = "<<(void*)&ptrChar<<endl//Direccion de ptrChar
-        <<"*ptrChar  = "<<*ptrChar<<endl<<endl;//Dato contenido en direccion contenida en ptrChar
+    cout<<"P.muestraTuEstado() => ";P.muestraTuEstado();cout<<endl;//Estado de P
+    cout<<"&P\t\t= "<<&P<<endl//Direccion de P
+        <<"ptrPunto2D      = "<<ptrPunto2D<<endl//Contenido en ptrInt
+        <<"&ptrFraccion    = "<<&ptrPunto2D<<endl;//Direccion de ptrInt
+    cout<<"ptrPunto2D->muestraTuEstado() => ";ptrPunto2D->muestraTuEstado();cout<<endl<<endl;//Estado de Objeto Punto2D apuntado por ptrPunto2D
+
+    cout<<"C.muestraTuEstado() => ";C.muestraTuEstado();cout<<endl;//Estado de C
+    cout<<"&C\t\t= "<<&C<<endl//Direccion de C
+        <<"ptrComplejo     = "<<ptrComplejo<<endl//Contenido en ptrFloat
+        <<"&ptrComplejo    = "<<&ptrComplejo<<endl;//Direccion de ptrFloat
+    cout<<"ptrComplejo->muestraTuEstado() => ";ptrComplejo->muestraTuEstado();cout<<endl<<endl;//Estado de Objeto Complejo apuntado por ptrComplejo
+
+    cout<<"F.muestraTuEstado() => ";F.muestraTuEstado();cout<<endl;//Estado de F
+    cout<<"&F\t\t= "<<&F<<endl//Direccion de F
+        <<"ptrFraccion     = "<<ptrFraccion<<endl//Contenido en ptrChar
+        <<"&ptrFraccion    = "<<&ptrFraccion<<endl;//Direccion de ptrChar
+    cout<<"ptrFraccion->muestraTuEstado() => ";ptrFraccion->muestraTuEstado();cout<<endl<<endl;//Estado de Objeto Fraccion apuntado por ptrFraccion
 
     return 0;
 }
