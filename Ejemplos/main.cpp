7dc61310b43a18281ce0c81c93b27ede289581b1
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-06-18 08:19:52 -0600

    Plantilla de una funcion

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -1,64 +1,52 @@
 #include <iostream>
-#include <stdio.h>
-#include "Persona.h"
-#include "Empleado.h"
-#include "Profesor.h"
+#include "Punto2D.h"
+#include "Complejo.h"
+#include "Fraccion.h"
+#include "Monomio.h"
 using namespace std;
 
-int main(void){
-    Persona P;
-    Empleado E;
-    Profesor M;
-    Persona* ptrP;
-    Empleado* ptrE;
+template<typename T>
+T suma(T a, T b);
 
-    ptrP = &P;
-    cout<<"P con ptrP"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl;
-    cout<<endl<<"P con ptrP"<<endl;
-    ptrP->pideleAlUsuarioTuEstado(); cout<<endl;
-    cout<<"P con ptrP"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl<<endl;
-    system("pause");                cout<<endl<<endl;
-    system("cls");
+int main(void) {
+    int aI=2147483646, bI=1, cI;
+    cI = suma(aI, bI);
+    cout << aI << " + " << bI << " = " << cI << endl << endl;
 
-    ptrP = &E;
-    cout<<"E con ptrP"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl;
-    cout<<endl<<"E con ptrP"<<endl;
-    ptrP->pideleAlUsuarioTuEstado(); cout<<endl;
-    cout<<"E con ptrP"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl;
-    system("pause");                cout<<endl<<endl;
-    system("cls");
+    long long aL=9223372036854775806, bL=1, cL;
+    cL = suma(aL, bL);
+    cout << aL << " + " << bL << " = " << cL << endl << endl;
 
-    ptrP = &M;
-    cout<<"M con ptrP"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl;
-    cout<<endl<<"M con ptrP"<<endl;
-    ptrP->pideleAlUsuarioTuEstado(); cout<<endl;
-    cout<<"M con ptrP"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl;
-    system("pause");                cout<<endl<<endl;
-    system("cls");
+    float aF=2.40282e+038, bF=1e+038, cF;
+    cF = suma(aF, bF);
+    cout << aF << " + " << bF << " = " << cF << endl << endl;
 
-    ptrP = &E;
-    cout<<"E con ptrE"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl;
-    cout<<endl<<"E con ptrE"<<endl;
-    ptrP->pideleAlUsuarioTuEstado(); cout<<endl;
-    cout<<"E con ptrE"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl;
-    system("pause");                cout<<endl<<endl;
-    system("cls");
+    double aD=0.79769e+308, bD=1e+308, cD;
+    cD = suma(aD, bD);
+    cout << aD << " + " << bD << " = " << cD << endl << endl;
 
-    ptrP = &M;
-    cout<<"M con ptrE"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl;
-    cout<<endl<<"M con ptrE"<<endl;
-    ptrP->pideleAlUsuarioTuEstado(); cout<<endl;
-    cout<<"M con ptrE"<<endl;
-    ptrP->muestraTuEstado();         cout<<endl;
+    Punto2D AP(1,2), BP(3,4), CP;
+    CP = suma(AP, BP);
+    cout << AP << " + " << BP << " = " << CP << endl << endl;
+
+    Complejo AC(1,2), BC(3,4), CC;
+    CC = suma(AC, BC);
+    cout << AC << " + " << BC << " = " << CC << endl << endl;
+
+    Fraccion AF(1,2), BF(3,4), CF;
+    CF = suma(AF, BF);
+    cout << AF << " + " << BF << " = " << CF << endl << endl;
+
+    Monomio AM(1,2), BM(3,4), CM;
+    CM = suma(AM, BM);
+    cout << AM << " + " << BM << " = " << CM << endl << endl;
 
     return 0;
+}
+
+template<typename T>
+T suma(T a, T b){
+    T c;
+    c = a+b;
+    return c;
 }
