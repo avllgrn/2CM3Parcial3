26bdba8e63afe28b23ea148ee4c8f6fe4eb98a8c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-06-18 08:20:53 -0600

    Valores maximos y minimos de tipos de datos

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -1,52 +1,33 @@
 #include <iostream>
-#include "Punto2D.h"
-#include "Complejo.h"
-#include "Fraccion.h"
-#include "Monomio.h"
+#include <limits>
+
 using namespace std;
 
 template<typename T>
-T suma(T a, T b);
-
-int main(void) {
-    int aI=2147483646, bI=1, cI;
-    cI = suma(aI, bI);
-    cout << aI << " + " << bI << " = " << cI << endl << endl;
-
-    long long aL=9223372036854775806, bL=1, cL;
-    cL = suma(aL, bL);
-    cout << aL << " + " << bL << " = " << cL << endl << endl;
-
-    float aF=2.40282e+038, bF=1e+038, cF;
-    cF = suma(aF, bF);
-    cout << aF << " + " << bF << " = " << cF << endl << endl;
-
-    double aD=0.79769e+308, bD=1e+308, cD;
-    cD = suma(aD, bD);
-    cout << aD << " + " << bD << " = " << cD << endl << endl;
-
-    Punto2D AP(1,2), BP(3,4), CP;
-    CP = suma(AP, BP);
-    cout << AP << " + " << BP << " = " << CP << endl << endl;
-
-    Complejo AC(1,2), BC(3,4), CC;
-    CC = suma(AC, BC);
-    cout << AC << " + " << BC << " = " << CC << endl << endl;
-
-    Fraccion AF(1,2), BF(3,4), CF;
-    CF = suma(AF, BF);
-    cout << AF << " + " << BF << " = " << CF << endl << endl;
-
-    Monomio AM(1,2), BM(3,4), CM;
-    CM = suma(AM, BM);
-    cout << AM << " + " << BM << " = " << CM << endl << endl;
-
-    return 0;
+void showMinMax() {
+    cout << "min: " << numeric_limits<T>::min() << endl;
+    cout << "max: " << numeric_limits<T>::max() << endl;
+    cout << endl;
 }
 
-template<typename T>
-T suma(T a, T b){
-    T c;
-    c = a+b;
-    return c;
+int main(void) {
+    cout << "short:" << endl;
+    showMinMax<short>();
+    cout << "int:" << endl;
+    showMinMax<int>();
+    cout << "long:" << endl;
+    showMinMax<long>();
+    cout << "float:" << endl;
+    showMinMax<float>();
+    cout << "double:" << endl;
+    showMinMax<double>();
+    cout << "long double:" << endl;
+    showMinMax<long double>();
+    cout << "unsigned short:" << endl;
+    showMinMax<unsigned short>();
+    cout << "unsigned int:" << endl;
+    showMinMax<unsigned int>();
+    cout << "unsigned long:" << endl;
+    showMinMax<unsigned long>();
+    return 0;
 }
